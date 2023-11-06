9051116518626b1b2a2b8d448139c693bed41da8
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-11-06 17:39:57 +0100

    Update auto_ssh.sh

--- auto_ssh.sh
+++ auto_ssh.sh
@@ -3,29 +3,31 @@
 host_key=/etc/dropbear/dropbear_rsa_host_key
 host_key_bk=/data/auto_ssh/dropbear_rsa_host_key
 
-# 还原备份的SSH密钥
-if [ -f $host_key_bk ]; then
-    ln -sf $host_key_bk $host_key
-fi
+# Restore the host key.
+[ -f $host_key_bk ] && ln -sf $host_key_bk $host_key
 
-# 开启telnet、ssh、uart、boot_wait
+# Enable telnet, ssh, uart and boot_wait.
 [ "$(nvram get telnet_en)" = 0 ] && nvram set telnet_en=1 && nvram commit
 [ "$(nvram get ssh_en)" = 0 ] && nvram set ssh_en=1 && nvram commit
 [ "$(nvram get uart_en)" = 0 ] && nvram set uart_en=1 && nvram commit
-[ "$(nvram get boot_wait)" = "off" ]  && nvram set boot_wait=on && nvram commit
+[ "$(nvram get boot_wait)" = "off" ] && nvram set boot_wait=on && nvram commit
 
-[ "`uci -c /usr/share/xiaoqiang get xiaoqiang_version.version.CHANNEL`" != 'stable' ] && {
-	uci -c /usr/share/xiaoqiang set xiaoqiang_version.version.CHANNEL='stable' 
-    uci -c /usr/share/xiaoqiang commit xiaoqiang_version.version
+[ "$(uci -c /usr/share/xiaoqiang get xiaoqiang_version.version.CHANNEL)" != 'stable' ] && {
+uci -c /usr/share/xiaoqiang set xiaoqiang_version.version.CHANNEL='stable'
+uci -c /usr/share/xiaoqiang commit xiaoqiang_version.version 2>/dev/null
 }
 
-if [ -z "$(pidof dropbear)" -o -z "$(netstat -ntul | grep :22)" ]; then
-    sed -i 's/channel=.*/channel="debug"/g' /etc/init.d/dropbear
-    /etc/init.d/dropbear restart
-    /etc/init.d/dropbear enable
+channel=$(/sbin/uci get /usr/share/xiaoqiang/xiaoqiang_version.version.CHANNEL)
+if [ "$channel" = "release" ]; then
+   sed -i 's/channel=.*/channel="debug"/g' /etc/init.d/dropbear
 fi
 
-# 备份SSH密钥
+if [ -z "$(pidof dropbear)" -o -z "$(netstat -ntul | grep :22)" ]; then
+  /etc/init.d/dropbear restart 2>/dev/null
+  /etc/init.d/dropbear enable
+fi
+
+# Backup the host key.
 if [ ! -s $host_key_bk ]; then
     i=0
     while [ $i -le 30 ]
