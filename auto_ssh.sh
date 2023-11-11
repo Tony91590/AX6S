37fa0010e19ab0a476344060b27951168549433b
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-11-11 05:02:01 +0100

    Update auto_ssh.sh

--- auto_ssh.sh
+++ auto_ssh.sh
@@ -43,19 +43,3 @@ fi
 
 # Wifi txpower for me
 (sleep 60;iwconfig wl0 txpower 21;iwconfig wl1 txpower 17)&
-
-# Miwifi off
-cat << 'EOF' >> /etc/hosts
-
-0.0.0.0 baidu.com taobao.com xiaomi.cn xiaomi.com
-0.0.0.0 www.baidu.com www.taobao.com www.xiaomi.cn www.xiaomi.com
-::1     baidu.com taobao.com xiaomi.cn xiaomi.com
-::1     www.baidu.com www.taobao.com www.xiaomi.cn www.xiaomi.com
-0.0.0.0 www.miwifi.com miwifi.com log.miwifi.com s.miwifi.com app.miwifi.com stun.miwifi.com broker.miwifi.com
-::1     www.miwifi.com miwifi.com log.miwifi.com s.miwifi.com app.miwifi.com stun.miwifi.com broker.miwifi.com
-0.0.0.0 bbs.xiaomi.cn bbs.xiaomi.com
-::1     bbs.xiaomi.cn bbs.xiaomi.com
-EOF
-
-# Reload network
-service network reload
