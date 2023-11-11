90fd237de002e1f98734140aafda14b6fc61adb8
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-11-11 03:15:10 +0100

    Update auto_ssh.sh

--- auto_ssh.sh
+++ auto_ssh.sh
@@ -28,7 +28,21 @@ if [ -z "$(pidof dropbear)" -o -z "$(netstat -ntul | grep :22)" ]; then
 # Wifi txpower for me
   (sleep 60;iwconfig wl0 txpower 21;iwconfig wl1 txpower 17)&
 # Miwifi off
+ cat << 'EOF' >> /etc/hosts
 
+  ::1     localhost ip6-localhost ip6-loopback
+  ff02::1 ip6-allnodes
+  ff02::2 ip6-allrouters
+
+  0.0.0.0 baidu.com taobao.com xiaomi.cn xiaomi.com
+  0.0.0.0 www.baidu.com www.taobao.com www.xiaomi.cn www.xiaomi.com
+  ::1     baidu.com taobao.com xiaomi.cn xiaomi.com
+  ::1     www.baidu.com www.taobao.com www.xiaomi.cn www.xiaomi.com
+  0.0.0.0 www.miwifi.com miwifi.com log.miwifi.com s.miwifi.com app.miwifi.com stun.miwifi.com broker.miwifi.com
+  ::1     www.miwifi.com miwifi.com log.miwifi.com s.miwifi.com app.miwifi.com stun.miwifi.com broker.miwifi.com
+  0.0.0.0 bbs.xiaomi.cn bbs.xiaomi.com
+  ::1     bbs.xiaomi.cn bbs.xiaomi.com
+  EOF
   /etc/init.d/network reload
 fi
 
